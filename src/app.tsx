2b93961a4c92dbf22ef24843572008e2b7fe9727
Yi Cai <ycai@cyphercor.com>
2022-05-14 14:59:02 -0400

    build: init commit including configurations and file structures

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './app.less';
+
+const App: React.FC<Record<string, never>> = () => (
+  <div className="app">Welcome to Graphql App</div>
+);
+
+export default App;
